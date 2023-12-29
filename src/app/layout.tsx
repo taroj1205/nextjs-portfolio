1ad0cfd71cef28f15130bcda866673cffdb6d04a
taroj1205 <taroj1205@gmail.com>
2023-12-29 14:59:15 +1300

    update to add themes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { ThemeSwitcher } from "@/components/ThemeSwitcher";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,9 +19,10 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={`${inter.className} bg-[#f5f5f5] dark:bg-[#0b0d0e]`}>
-          <Providers>
-						<main className="p-6 min-h-[100svh]">{children}</main>
-					</Providers>
+				<Providers>
+					<ThemeSwitcher />
+					<main className="p-6 min-h-[100svh]">{children}</main>
+				</Providers>
 			</body>
 		</html>
 	);
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,14 +1,20 @@
 "use client";
 import { CssVarsProvider } from "@mui/joy";
-import { ColorModeScript, UIProvider } from "@yamada-ui/react";
+import { ColorModeScript, UIProvider, createColorModeManager, defaultConfig } from "@yamada-ui/react";
 import { ThemeProvider } from "next-themes";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
+  const colorModeManager = createColorModeManager("cookie");
+  
 	return (
-		<ThemeProvider attribute="class">
+		<ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 			<CssVarsProvider defaultMode="dark">
-				<UIProvider>
-					<ColorModeScript initialColorMode={"dark"} />
+				<UIProvider colorModeManager={colorModeManager}>
+					<ColorModeScript
+						type="cookie"
+						nonce="testing"
+						initialColorMode={defaultConfig.initialColorMode}
+					/>
 					{children}
 				</UIProvider>
 			</CssVarsProvider>
