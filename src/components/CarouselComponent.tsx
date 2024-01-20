0359a7a768262f88b88f7f152f9e3816b12db773
taroj1205 <taroj1205@gmail.com>
2024-01-21 11:19:00 +1300

    improve langtoggle and carousel

--- src/components/CarouselComponent.tsx
+++ src/components/CarouselComponent.tsx
@@ -29,14 +29,14 @@ const images = [
 
 
 export const CarouselComponent = () => {
-	// const size = useBreakpointValue({ base: "full", lg: "50%" });
+	const size = useBreakpointValue({ base: "50%", md: "full" });
 	return (
 		<Carousel
 			speed={50}
 			delay={2000}
 			autoplay
 			stopMouseEnterAutoplay={true}
-			slideSize={"full"}
+			slideSize={size}
 			className="max-w-4xl h-fit">
 			{images.map((image, index) => (
 				<CarouselSlide className="h-fit" key={index} as={Center}>
--- src/components/LangToggle.tsx
+++ src/components/LangToggle.tsx
@@ -1,31 +1,34 @@
-import { usePathname } from "@/lib/next-intl";
+import { usePathname, useRouter } from "@/lib/next-intl";
 import { useLocale } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 
 export const LangToggle = () => {
-  const pathname = usePathname();
-  const locale = useLocale();
-  return (
-    <div className="relative inline-block text-left">
-      <Link
-        href={locale === "en" ? "/ja" : "/en" + pathname}
-        className={`flex items-center justify-center w-fit p-1 mr-1 md:mr-0 rounded-full active:scale-95 duration-200 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50`}>
-        <Image
-          src="/svg/flag/jp.svg"
-          alt="Theme"
-          width={24}
-          height={24}
-          className="w-6 h-6 toENG"
-        />
-        <Image
-          src="/svg/flag/nz.svg"
-          alt="Theme"
-          width={24}
-          height={24}
-          className="w-6 h-6 toJPN"
-        />
-      </Link>
-    </div>
-    );
+	const pathname = usePathname();
+	const locale = useLocale();
+	const router = useRouter();
+	return (
+		<div className="relative inline-block text-left">
+			<button
+				onClick={() => {
+					router.replace(pathname, { locale: locale === "en" ? "ja" : "en" });
+				}}
+				className={`flex items-center justify-center w-fit p-1 mr-1 md:mr-0 rounded-full active:scale-95 duration-200 focus:border-blue-300 focus:ring focus:ring-blue-200 focus:ring-opacity-50`}>
+				<Image
+					src="/svg/flag/jp.svg"
+					alt="Theme"
+					width={24}
+					height={24}
+					className="w-6 h-6 toENG"
+				/>
+				<Image
+					src="/svg/flag/nz.svg"
+					alt="Theme"
+					width={24}
+					height={24}
+					className="w-6 h-6 toJPN"
+				/>
+			</button>
+		</div>
+	);
 };
