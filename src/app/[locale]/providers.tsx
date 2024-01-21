42106ed6a3a3d840a5a283218db222d76dd40d40
taroj1205 <taroj1205@gmail.com>
2024-01-22 09:42:33 +1300

    better useBreakPoint

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -1,11 +1,16 @@
 "use client";
-import { UIProvider, createColorModeManager } from "@yamada-ui/react";
+import { UIProvider, createColorModeManager, extendConfig } from "@yamada-ui/react";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
-  const colorModeManager = createColorModeManager("cookie");
+	const colorModeManager = createColorModeManager("cookie");
+	
+	const customConfig = extendConfig({
+		initialColorMode: "system",
+		breakpoint:	"up"
+	});
   
   return (
-			<UIProvider colorModeManager={colorModeManager}>
+			<UIProvider config={customConfig} colorModeManager={colorModeManager}>
 				{children}
 			</UIProvider>
 	);
