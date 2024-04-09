4a897cca921662eb71e7b295a4b0a2fb21436656
taroj1205 <taroj1205@gmail.com>
2024-04-09 16:46:13 +1200

    fix: make size optional

--- src/components/icons/socials/github.tsx
+++ src/components/icons/socials/github.tsx
@@ -4,7 +4,7 @@ import { Icon } from "@yamada-ui/fontawesome"
 import { memo } from "react"
 
 export const GitHubIcon = memo(
-  ({ size = 30, ...props }: { size: number | string }) => (
+  ({ size = 30, ...props }: { size?: number | string }) => (
     <Icon icon={faGithub} size={size} {...props} />
   ),
 )
