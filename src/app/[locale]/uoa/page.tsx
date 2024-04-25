914a3537a650b030f82cd37f3afddfe8a8d2fdf4
taroj1205 <taroj1205@gmail.com>
2024-04-26 09:30:46 +1200

    chore: update images in UoA page and carousel component

--- src/app/[locale]/uoa/page.tsx
+++ src/app/[locale]/uoa/page.tsx
@@ -3,13 +3,16 @@ import {
   Card,
   CardHeader,
   CardBody,
-  Image,
   Heading,
   Text,
   VStack,
 } from "@yamada-ui/react"
 import type { Metadata } from "next"
+import type { StaticImageData } from "next/image"
+import Image from "next/image"
 import Link from "next/link"
+import canvas from "../../../assets/uoa/canvas.webp"
+import logo from "../../../assets/uoa/logo.webp"
 import { getDictionary } from "lib/dictionaries"
 
 export const metadata: Metadata = {
@@ -22,44 +25,44 @@ const links = [
   {
     title: "UoA Homepage",
     description: "The homepage for UoA.",
-    thumbnail: "/images/uoa/logo.webp",
+    thumbnail: logo,
     video: false,
     owner: "University of Auckland",
     brief: "UoA",
     link: "https://www.auckland.ac.nz/",
-    logo: "/images/uoa/logo.webp",
+    logo: logo,
   },
   {
     title: "Student Services Online",
     description: "This is the SSO for UoA.",
-    thumbnail: "/images/uoa/logo.webp",
+    thumbnail: logo,
     video: false,
     owner: "University of Auckland",
     brief: "UoA",
     link: "https://www.student.auckland.ac.nz/",
-    logo: "/images/uoa/logo.webp",
+    logo: logo,
   },
   {
     title: "Canvas",
     description: "This is the Canvas for UoA.",
-    thumbnail: "/images/uoa/canvas.webp",
+    thumbnail: canvas,
     video: false,
     owner: "University of Auckland",
     brief: "UoA",
     link: "https://canvas.auckland.ac.nz/",
-    logo: "/images/uoa/canvas.webp",
+    logo: canvas,
   },
 ]
 
-interface Link {
+interface LinkProps {
   title: string
   description: string
-  thumbnail: string
+  thumbnail: StaticImageData
   video: boolean
   owner: string
   brief: string
   link: string
-  logo: string
+  logo: StaticImageData
 }
 
 export default function Home({
@@ -75,7 +78,7 @@ export default function Home({
       </Heading>
       <Text>{header.uoa.description}</Text>
       <Wrap gap="md">
-        {links.map((link: Link) => (
+        {links.map((link: LinkProps) => (
           <Card
             as={Link}
             href={link.link}
@@ -88,12 +91,20 @@ export default function Home({
             variant="outline"
             _hover={{ bg: ["gray.100", "gray.800"] }}
           >
-            <CardHeader p="0" justifyContent="center">
+            <CardHeader
+              p="0"
+              justifyContent="center"
+              rounded="md"
+              overflow="hidden"
+            >
               <Image
+                style={{
+                  objectFit: "cover",
+                  width: "100%",
+                  height: "100%",
+                }}
                 src={link.thumbnail}
                 alt={link.description}
-                w="full"
-                rounded="md"
               />
             </CardHeader>
 
